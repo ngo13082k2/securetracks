e517feec18e98ed790b6a8b89276e5e88a7ac149
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2025-03-16 16:16:56 +0700

    repository

--- src/main/java/org/example/securetracks/service/IBottleQrCodeService.java
+++ src/main/java/org/example/securetracks/service/IBottleQrCodeService.java
@@ -3,8 +3,13 @@ package org.example.securetracks.service;
 import org.example.securetracks.model.BottleQrCode;
 import org.example.securetracks.response.BottleQrCodeResponse;
 
+import java.util.List;
+import java.util.Map;
+
 public interface IBottleQrCodeService {
     void generateQrCodesForAllDeliveries();
     BottleQrCodeResponse getBottleInfoByQrCode(String qrCode);
     BottleQrCode getBottleQrCodeByQrCode(String qrCode);
+
+    List<Map<String, Object>> getQRCodesByDeliveryId(Long deliveryId);
 }
--- src/main/java/org/example/securetracks/service/IDeliveryService.java
+++ src/main/java/org/example/securetracks/service/IDeliveryService.java
@@ -1,10 +1,12 @@
 package org.example.securetracks.service;
 
 import org.example.securetracks.dto.DeliveryDto;
+import org.example.securetracks.request.CreateDeliveryRequest;
 
 import java.util.List;
 
 public interface IDeliveryService {
     DeliveryDto create(DeliveryDto dto);
     List<DeliveryDto> saveAll(List<DeliveryDto> deliveryDtos);
+    public String createDelivery(CreateDeliveryRequest request);
 }
